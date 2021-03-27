7d37d17216a8caf8d3d8cf1158581bc5b8d61227
JnavioT <jose_navio@hotmail.com>
2021-03-26 21:17:07 -0500

    Chapter10

---
+++ C10_Approaching_text_classification_regression/src/logic_regression_sentiment_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+from nltk.tokenize import word_tokenize
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn import model_selection
+from sklearn.feature_extraction.text import CountVectorizer
+
+if __name__ == "__main__":
+    
